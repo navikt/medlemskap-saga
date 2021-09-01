df0b5baeed6228291236e1f4a28270bf94ee1a26
hans-jacob Melby <hans-jacob.melby@nav.no>
2021-09-01 14:55:57 +0200

    test

--- src/test/kotlin/no/nav/medlemskap/sykepenger/lytter/jakson/JaksonParserTest.kt
+++ src/test/kotlin/no/nav/medlemskap/sykepenger/lytter/jakson/JaksonParserTest.kt
@@ -1,14 +1,12 @@
 package no.nav.medlemskap.sykepenger.lytter.jakson
 
-import com.fasterxml.jackson.databind.JsonNode
+
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
-import java.time.LocalDate
-
 
 class JaksonParserTest {
 
-    @Test
+    @Test()
     fun `parse Json String`() {
         val fileContent = this::class.java.classLoader.getResource("sampleVurdering.json").readText(Charsets.UTF_8)
         val JsonNode = JaksonParser().parse(fileContent)
