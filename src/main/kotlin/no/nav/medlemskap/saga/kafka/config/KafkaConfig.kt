edd6a54d06a42d0c2e7be7148932e3c901f02b14
hans-jacob Melby <hans-jacob.melby@nav.no>
2021-11-23 16:34:45 +0100

    readme endringer og opplegg for å kjøre komponent lokalt

--- src/main/kotlin/no/nav/medlemskap/saga/kafka/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/medlemskap/saga/kafka/config/KafkaConfig.kt
@@ -2,6 +2,7 @@ package no.nav.medlemskap.saga.kafka.config
 
 import no.nav.medlemskap.saga.config.Configuration
 import no.nav.medlemskap.saga.config.Environment
+import no.nav.medlemskap.saga.config.EnvironmentKey
 import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.consumer.KafkaConsumer
@@ -28,7 +29,22 @@ open class KafkaConfig(
         //ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "latest",
     ) + securityStrategy.securityConfig()
 
-    fun createConsumer() = KafkaConsumer<String, String>(inst2Config())
+    fun inst2Configdev() = mapOf(
+        CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG to "localhost:29092",
+        ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+        CommonClientConfigs.CLIENT_ID_CONFIG to Configuration.KafkaConfig().clientId,
+        ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+        ConsumerConfig.GROUP_ID_CONFIG to Configuration.KafkaConfig().groupID,
+
+    )
+
+
+    fun createConsumer():KafkaConsumer<String, String>  {
+        return if (EnvironmentKey.IS_LOCAL.equals("true"))
+            KafkaConsumer<String, String>(inst2Configdev())
+        else KafkaConsumer<String, String>(inst2Config())
+
+    }
 
     interface SecurityStrategy {
         fun securityConfig(): Map<String, String>
