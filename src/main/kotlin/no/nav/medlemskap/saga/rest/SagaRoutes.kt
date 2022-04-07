cef27298cfd9bbf035e47a8620742797f6d8b888
hans-jacob Melby <hans-jacob.melby@nav.no>
2022-04-07 12:09:44 +0200

    Liten endring i sikkerhet med nais config

--- src/main/kotlin/no/nav/medlemskap/saga/rest/SagaRoutes.kt
+++ src/main/kotlin/no/nav/medlemskap/saga/rest/SagaRoutes.kt
@@ -4,13 +4,13 @@ import io.ktor.application.call
 import io.ktor.auth.*
 import io.ktor.auth.jwt.*
 import io.ktor.features.*
+import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.respond
-import io.ktor.routing.Routing
-import io.ktor.routing.post
-import io.ktor.routing.route
+import io.ktor.routing.*
 import mu.KotlinLogging
 import java.util.*
+import kotlin.text.get
 
 private val secureLogger = KotlinLogging.logger("tjenestekall")
 private val logger = KotlinLogging.logger { }
@@ -19,13 +19,40 @@ fun Routing.sagaRoutes() {
         authenticate("azureAuth") {
             post {
                 logger.info("kall autentisert")
+
                 val callerPrincipal: JWTPrincipal = call.authentication.principal()!!
                 val azp = callerPrincipal.payload.getClaim("azp").asString()
                 secureLogger.info("EvalueringRoute: azp-claim i principal-token: {}", azp)
                 val callId = call.callId ?: UUID.randomUUID().toString()
                 val udiRequest = call.receive<Void>()
                 call.respond("soon to contain vurdering")
+                //om bruker ikke har korrekt rolle:
+                call.respond(HttpStatusCode.Forbidden,"soon to contain vurdering")
+            }
+            get() {
+                logger.info("kall autentisert")
+                val callerPrincipal: JWTPrincipal = call.authentication.principal()!!
+                val azp = callerPrincipal.payload.getClaim("azp").asString()
+                secureLogger.info("EvalueringRoute: azp-claim i principal-token: {}", azp)
+                val callId = call.callId ?: UUID.randomUUID().toString()
+                val vurderingRequest = call.receive<Void>()
+                val vurdering = null;
+
+                if(vurdering!=null){
+                    /*
+                    * scenario : Client asks for a validation we find
+                    * */
+                    call.respond("soon to contain vurdering")
+                }
+                else{
+                    /*
+                * scenario : Client asks for a validation we can not find
+                * */
+                    call.respond(HttpStatusCode.NotFound)
+                }
+
             }
         }
     }
+
 }
