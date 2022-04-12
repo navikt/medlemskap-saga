f07010b5c254a9652a3494a22d4a110338ad01cf
hans-jacob Melby <hans-jacob.melby@nav.no>
2022-04-12 14:52:37 +0200

    prøver impl av nytt api

--- src/main/kotlin/no/nav/medlemskap/saga/rest/SagaRoutes.kt
+++ src/main/kotlin/no/nav/medlemskap/saga/rest/SagaRoutes.kt
@@ -17,47 +17,23 @@ import java.util.*
 private val secureLogger = KotlinLogging.logger("tjenestekall")
 private val logger = KotlinLogging.logger { }
 fun Routing.sagaRoutes(service: SagaService) {
-    route("/vurdering2") {
+    route("/findVureringerByFnr") {
         authenticate("azureAuth") {
             post{
-                logger.info("kall autentisert, url : /vurdering")
+                logger.info("kall autentisert, url : /findVureringerByFnr")
 
                 val callerPrincipal: JWTPrincipal = call.authentication.principal()!!
                 val azp = callerPrincipal.payload.getClaim("azp").asString()
                 secureLogger.info("EvalueringRoute: azp-claim i principal-token: {}", azp)
                 val callId = call.callId ?: UUID.randomUUID().toString()
-                try {
-                    val request = call.receive<Request>()
+                try{
+                    val request = call.receive<FnrRequest>()
                     val vurderinger = service.finnAlleVurderingerForFnr(request.fnr)
-                    secureLogger.info {
-                        "Antall vurderinger found in db for ${request.fnr} is : ${vurderinger.size}"
-                        kv("fnr", request.fnr)
-                        kv("callId", callId)
-                    }
-                    val periode = Periode(request.periode.fom, request.periode.tom)
-                    val vurdering = filterVurderinger(vurderinger, periode, request.fnr)
-                    if (vurdering.isEmpty) {
-                        secureLogger.info {
-                            "cache miss for  ${request.fnr}"
-                            kv("fnr", request.fnr)
-                            kv("callId", callId)
-                            kv("periode", periode)
-                        }
-                        call.respond(HttpStatusCode.NotFound)
-                    } else {
-                        val response = vurdering.get()
-                        secureLogger.info {
-                            "cache hit for  ${request.fnr}"
-                            kv("fnr", request.fnr)
-                            kv("callId", callId)
-                            kv("periode", periode)
-                            kv("soknadId", response.soknadId)
-                            kv("id", response.id)
-                        }
-                        call.respond(HttpStatusCode.OK, response.json)
-                    }
-                } catch (t: Throwable) {
+                    call.respond(vurderinger.map { mapToFnrResponse(it) })
+                }
+                catch (t:Throwable){
                     call.respond(t.stackTrace)
+
                 }
             }
         }
