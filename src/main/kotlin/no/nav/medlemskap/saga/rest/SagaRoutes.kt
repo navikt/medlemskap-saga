e451a82aaed6027b9cd6aeae61b96a9ae3bfad1f
hans-jacob Melby <hans-jacob.melby@nav.no>
2022-04-12 14:25:37 +0200

    2 identiske routes

--- src/main/kotlin/no/nav/medlemskap/saga/rest/SagaRoutes.kt
+++ src/main/kotlin/no/nav/medlemskap/saga/rest/SagaRoutes.kt
@@ -17,6 +17,51 @@ import java.util.*
 private val secureLogger = KotlinLogging.logger("tjenestekall")
 private val logger = KotlinLogging.logger { }
 fun Routing.sagaRoutes(service: SagaService) {
+    route("/vurdering2") {
+        authenticate("azureAuth") {
+            post{
+                logger.info("kall autentisert, url : /vurdering")
+
+                val callerPrincipal: JWTPrincipal = call.authentication.principal()!!
+                val azp = callerPrincipal.payload.getClaim("azp").asString()
+                secureLogger.info("EvalueringRoute: azp-claim i principal-token: {}", azp)
+                val callId = call.callId ?: UUID.randomUUID().toString()
+                try {
+                    val request = call.receive<Request>()
+                    val vurderinger = service.finnAlleVurderingerForFnr(request.fnr)
+                    secureLogger.info {
+                        "Antall vurderinger found in db for ${request.fnr} is : ${vurderinger.size}"
+                        kv("fnr", request.fnr)
+                        kv("callId", callId)
+                    }
+                    val periode = Periode(request.periode.fom, request.periode.tom)
+                    val vurdering = filterVurderinger(vurderinger, periode, request.fnr)
+                    if (vurdering.isEmpty) {
+                        secureLogger.info {
+                            "cache miss for  ${request.fnr}"
+                            kv("fnr", request.fnr)
+                            kv("callId", callId)
+                            kv("periode", periode)
+                        }
+                        call.respond(HttpStatusCode.NotFound)
+                    } else {
+                        val response = vurdering.get()
+                        secureLogger.info {
+                            "cache hit for  ${request.fnr}"
+                            kv("fnr", request.fnr)
+                            kv("callId", callId)
+                            kv("periode", periode)
+                            kv("soknadId", response.soknadId)
+                            kv("id", response.id)
+                        }
+                        call.respond(HttpStatusCode.OK, response.json)
+                    }
+                } catch (t: Throwable) {
+                    call.respond(t.stackTrace)
+                }
+            }
+        }
+    }
     route("/vurdering") {
         authenticate("azureAuth") {
             post{
--- src/test/kotlin/no/nav/medlemskap/sykepenger/lytter/jakson/JaksonParserTest.kt
+++ src/test/kotlin/no/nav/medlemskap/sykepenger/lytter/jakson/JaksonParserTest.kt
@@ -1,11 +1,10 @@
 package no.nav.medlemskap.sykepenger.lytter.jakson
 
 
-import com.fasterxml.jackson.databind.ObjectMapper
-import no.nav.medlemskap.saga.rest.FnrRequest
-import no.nav.medlemskap.saga.rest.objectMapper
+import no.nav.medlemskap.saga.rest.*
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
+import java.time.LocalDate
 
 class JaksonParserTest {
 
@@ -19,6 +18,12 @@ class JaksonParserTest {
     @Test
     fun print(){
         println(objectMapper.writeValueAsString(FnrRequest("12344")))
+    }
+    @Test
+    fun printRequest(){
+        println(objectMapper.writeValueAsString(
+            Request("1234", LocalDate.now(), InputPeriode(LocalDate.now(),LocalDate.now()),Ytelse.SYKEPENGER)
+        ))
     }
 
 }
