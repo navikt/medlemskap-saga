f5f454f96943d35fc2ff0a353b5d518eb067e579
hans-jacob Melby <hans-jacob.melby@nav.no>
2022-04-12 10:55:25 +0200

    nytt forsøk

--- src/main/kotlin/no/nav/medlemskap/saga/rest/SagaRoutes.kt
+++ src/main/kotlin/no/nav/medlemskap/saga/rest/SagaRoutes.kt
@@ -18,72 +18,76 @@ private val secureLogger = KotlinLogging.logger("tjenestekall")
 private val logger = KotlinLogging.logger { }
 fun Routing.sagaRoutes(service: SagaService) {
     route("/demo") {
-        get {
-            call.respondText("dette er en test på get", ContentType.Text.Plain, HttpStatusCode.OK)
-        }
-    }
-    /*
-    route("/") {
         authenticate("azureAuth") {
-            post("vurdering") {
-                logger.info("kall autentisert, url : /vurdering")
-
-                val callerPrincipal: JWTPrincipal = call.authentication.principal()!!
-                val azp = callerPrincipal.payload.getClaim("azp").asString()
-                secureLogger.info("EvalueringRoute: azp-claim i principal-token: {}", azp)
-                val callId = call.callId ?: UUID.randomUUID().toString()
-                try {
-                    val request = call.receive<Request>()
-                    val vurderinger = service.finnAlleVurderingerForFnr(request.fnr)
-                    secureLogger.info {
-                        "Antall vurderinger found in db for ${request.fnr} is : ${vurderinger.size}"
-                        kv("fnr", request.fnr)
-                        kv("callId", callId)
-                    }
-                    val periode = Periode(request.periode.fom, request.periode.tom)
-                    val vurdering = filterVurderinger(vurderinger, periode, request.fnr)
-                    if (vurdering.isEmpty) {
-                        secureLogger.info {
-                            "cache miss for  ${request.fnr}"
-                            kv("fnr", request.fnr)
-                            kv("callId", callId)
-                            kv("periode", periode)
-                        }
-                        call.respond(HttpStatusCode.NotFound)
-                    } else {
-                        val response = vurdering.get()
-                        secureLogger.info {
-                            "cache hit for  ${request.fnr}"
-                            kv("fnr", request.fnr)
-                            kv("callId", callId)
-                            kv("periode", periode)
-                            kv("soknadId", response.soknadId)
-                            kv("id", response.id)
-                        }
-                        call.respond(HttpStatusCode.OK, response.json)
-                    }
-                } catch (t: Throwable) {
-                    call.respond(t.stackTrace)
-                }
-            }
-            post("findVureringerByFnr") {
-                logger.info("kall autentisert")
-
-                val callerPrincipal: JWTPrincipal = call.authentication.principal()!!
-                val azp = callerPrincipal.payload.getClaim("azp").asString()
-                secureLogger.info("EvalueringRoute: azp-claim i principal-token: {}", azp)
-                val callId = call.callId ?: UUID.randomUUID().toString()
-                try {
-                    val request = call.receive<FnrRequest>()
-                    val vurderinger = service.finnAlleVurderingerForFnr(request.fnr)
-                    call.respond(vurderinger.map { mapToFnrResponse(it) })
-                } catch (t: Throwable) {
-                    call.respond(t.stackTrace)
-
-                }
+            get {
+                call.respondText("dette er en test på get", ContentType.Text.Plain, HttpStatusCode.OK)
             }
         }
     }
-
-     */
 }
+
+/*
+        route("/") {
+            authenticate("azureAuth") {
+                post("vurdering") {
+                    logger.info("kall autentisert, url : /vurdering")
+
+                    val callerPrincipal: JWTPrincipal = call.authentication.principal()!!
+                    val azp = callerPrincipal.payload.getClaim("azp").asString()
+                    secureLogger.info("EvalueringRoute: azp-claim i principal-token: {}", azp)
+                    val callId = call.callId ?: UUID.randomUUID().toString()
+                    try {
+                        val request = call.receive<Request>()
+                        val vurderinger = service.finnAlleVurderingerForFnr(request.fnr)
+                        secureLogger.info {
+                            "Antall vurderinger found in db for ${request.fnr} is : ${vurderinger.size}"
+                            kv("fnr", request.fnr)
+                            kv("callId", callId)
+                        }
+                        val periode = Periode(request.periode.fom, request.periode.tom)
+                        val vurdering = filterVurderinger(vurderinger, periode, request.fnr)
+                        if (vurdering.isEmpty) {
+                            secureLogger.info {
+                                "cache miss for  ${request.fnr}"
+                                kv("fnr", request.fnr)
+                                kv("callId", callId)
+                                kv("periode", periode)
+                            }
+                            call.respond(HttpStatusCode.NotFound)
+                        } else {
+                            val response = vurdering.get()
+                            secureLogger.info {
+                                "cache hit for  ${request.fnr}"
+                                kv("fnr", request.fnr)
+                                kv("callId", callId)
+                                kv("periode", periode)
+                                kv("soknadId", response.soknadId)
+                                kv("id", response.id)
+                            }
+                            call.respond(HttpStatusCode.OK, response.json)
+                        }
+                    } catch (t: Throwable) {
+                        call.respond(t.stackTrace)
+                    }
+                }
+                post("findVureringerByFnr") {
+                    logger.info("kall autentisert")
+
+                    val callerPrincipal: JWTPrincipal = call.authentication.principal()!!
+                    val azp = callerPrincipal.payload.getClaim("azp").asString()
+                    secureLogger.info("EvalueringRoute: azp-claim i principal-token: {}", azp)
+                    val callId = call.callId ?: UUID.randomUUID().toString()
+                    try {
+                        val request = call.receive<FnrRequest>()
+                        val vurderinger = service.finnAlleVurderingerForFnr(request.fnr)
+                        call.respond(vurderinger.map { mapToFnrResponse(it) })
+                    } catch (t: Throwable) {
+                        call.respond(t.stackTrace)
+
+                    }
+                }
+            }
+        }
+
+         */
+
