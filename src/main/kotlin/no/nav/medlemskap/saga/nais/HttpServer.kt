a5c9339637228ddb920b79e8d86f6ab3e2df94b5
Stian S. Douzette <stians.douzette@nav.no>
2022-11-08 08:03:37 +0100

    oppgraderer ktor

--- src/main/kotlin/no/nav/medlemskap/saga/nais/HttpServer.kt
+++ src/main/kotlin/no/nav/medlemskap/saga/nais/HttpServer.kt
@@ -1,20 +1,27 @@
 package no.nav.medlemskap.saga.nais
+import com.fasterxml.jackson.core.util.DefaultIndenter
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter
 import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.MapperFeature
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import io.ktor.application.*
-import io.ktor.auth.*
-import io.ktor.auth.jwt.*
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
-import io.ktor.client.features.json.*
-import io.ktor.features.*
 import io.ktor.http.*
-import io.ktor.jackson.*
-import io.ktor.metrics.micrometer.*
+import io.ktor.serialization.jackson.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.plugins.callid.*
+import io.ktor.server.plugins.callloging.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.server.metrics.micrometer.*
 
-import io.ktor.routing.*
+import org.slf4j.event.Level
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.micrometer.prometheus.PrometheusMeterRegistry
@@ -22,6 +29,7 @@ import io.prometheus.client.exporter.common.TextFormat
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.withContext
+import no.nav.medlemskap.saga.MDC_CALL_ID
 import no.nav.medlemskap.saga.config.*
 import no.nav.medlemskap.saga.config.JwtConfig.Companion.REALM
 import no.nav.medlemskap.saga.lytter.Metrics
@@ -33,6 +41,7 @@ import no.nav.medlemskap.saga.service.SagaService
 import org.apache.http.impl.conn.SystemDefaultRoutePlanner
 import java.io.Writer
 import java.net.ProxySelector
+import java.util.*
 
 fun createHttpServer(consumeJob: Job) = embeddedServer(Netty, applicationEngineEnvironment {
     val useAuthentication: Boolean = true
@@ -42,6 +51,16 @@ fun createHttpServer(consumeJob: Job) = embeddedServer(Netty, applicationEngineE
 
     connector { port = 8080 }
     module {
+
+        install(CallId) {
+            header(MDC_CALL_ID)
+            generate { UUID.randomUUID().toString() }
+        }
+
+        install(CallLogging) {
+            level = Level.INFO
+            callIdMdc(MDC_CALL_ID)
+        }
 
         install(MicrometerMetrics) {
             registry = Metrics.registry
@@ -78,22 +97,6 @@ suspend fun writeMetrics004(writer: Writer, registry: PrometheusMeterRegistry) {
         kotlin.runCatching {
             TextFormat.write004(writer, registry.prometheusRegistry.metricFamilySamples())
         }
-    }
-}
-internal val apacheHttpClient = HttpClient(Apache) {
-    install(JsonFeature) {
-        serializer = JacksonSerializer {
-            this.registerModule(JavaTimeModule())
-                .configure(SerializationFeature.INDENT_OUTPUT, true)
-                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
-                .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS, true)
-        }
-    }
-
-    engine {
-        socketTimeout = 45000
-
-        customizeClient { setRoutePlanner(SystemDefaultRoutePlanner(ProxySelector.getDefault())) }
     }
 }
 
