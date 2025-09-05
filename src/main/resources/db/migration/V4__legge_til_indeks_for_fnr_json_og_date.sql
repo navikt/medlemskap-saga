09490557ec29550da6121f5452ca19df6f4372e7
Kieu Nguyen <kieunguyen@NO-FV9YQ6X7NL.local>
2025-09-05 10:47:57 +0200

    Opprettet indekser for fnr, json og date kolonnene når det ikke er opprettet fra før. Bruker idempotent spørring for at det som er i usynk ikke skal kollidere.

---
+++ src/main/resources/db/migration/V4__legge_til_indeks_for_fnr_json_og_date.sql
@@ -0,0 +1,3 @@
+CREATE INDEX IF NOT EXISTS fnr_index ON public.vurdering USING btree ((((json -> 'datagrunnlag'::text) ->> 'fnr'::text)))
+CREATE INDEX IF NOT EXISTS jsonb_index ON public.vurdering USING gin (json)
+CREATE INDEX IF NOT EXISTS date_index ON public.vurdering USING btree (date);
