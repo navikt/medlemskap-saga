a44c76afc863661a2fa4c358ee5650201ce46765
Kieu Nguyen <kieunguyen@NO-FV9YQ6X7NL.local>
2025-10-02 14:40:00 +0200

    Fikse opp i alter spørring

--- src/main/resources/db/migration/V8__alter_table_vurdering.sql
+++ src/main/resources/db/migration/V8__alter_table_vurdering.sql
@@ -1,13 +1,12 @@
-ALTER TABLE vurdering_analyse
-    RENAME COLUMN foersteDagForYtelse TO foerste_dag_for_ytelse,
-    RENAME COLUMN startDatoForYtelse TO start_dato_for_ytelse,
-    RENAME COLUMN nyeSpoersmaal TO nye_spoersmaal,
-    RENAME COLUMN antallDagerMedSykmelding TO antall_dager_med_sykmelding,
-    RENAME COLUMN arbeidUtenforNorge TO arbeid_utenfor_norge,
-    RENAME COLUMN utfoertArbeidUtenforNorgeTag TO utfoert_arbeid_utenfor_norge,
-    RENAME COLUMN oppholdUtenforEOSTag TO opphold_utenfor_eos,
-    RENAME COLUMN oppholdUtenforNorgeTag TO opphold_utenfor_norge,
-    RENAME COLUMN oppholdstillatelseOppgittTag TO oppholdstillatelse_oppgitt,
-    RENAME COLUMN oppholdstillatelseUDIFom TO oppholdstillatelse_udi_fom,
-    RENAME COLUMN oppholdstillatelseUDITom TO oppholdstillatelse_udi_tom,
-    RENAME COLUMN oppholdstillatelseUDIType TO oppholdstillatelse_udi_type;
+ALTER TABLE vurdering_analyse RENAME COLUMN foersteDagForYtelse TO foerste_dag_for_ytelse;
+ALTER TABLE vurdering_analyse RENAME COLUMN startDatoForYtelse TO start_dato_for_ytelse;
+ALTER TABLE vurdering_analyse RENAME COLUMN nyeSpoersmaal TO nye_spoersmaal;
+ALTER TABLE vurdering_analyse RENAME COLUMN antallDagerMedSykmelding TO antall_dager_med_sykmelding;
+ALTER TABLE vurdering_analyse RENAME COLUMN arbeidUtenforNorge TO arbeid_utenfor_norge;
+ALTER TABLE vurdering_analyse RENAME COLUMN utfoertArbeidUtenforNorgeTag TO utfoert_arbeid_utenfor_norge;
+ALTER TABLE vurdering_analyse RENAME COLUMN oppholdUtenforEOSTag TO opphold_utenfor_eos;
+ALTER TABLE vurdering_analyse RENAME COLUMN oppholdUtenforNorgeTag TO opphold_utenfor_norge;
+ALTER TABLE vurdering_analyse RENAME COLUMN oppholdstillatelseOppgittTag TO oppholdstillatelse_oppgitt;
+ALTER TABLE vurdering_analyse RENAME COLUMN oppholdstillatelseUDIFom TO oppholdstillatelse_udi_fom;
+ALTER TABLE vurdering_analyse RENAME COLUMN oppholdstillatelseUDITom TO oppholdstillatelse_udi_tom;
+ALTER TABLE vurdering_analyse RENAME COLUMN oppholdstillatelseUDIType TO oppholdstillatelse_udi_type;
