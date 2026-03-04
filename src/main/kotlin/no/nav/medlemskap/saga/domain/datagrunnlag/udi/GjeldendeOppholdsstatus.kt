f1c1b158cc91ef8acb408071183c375240ba94a2
Leggenn <111862499+Leggenn@users.noreply.github.com>
2026-03-04 09:10:42 +0100

    Utvider GjeldendeOppholdsstatus

--- src/main/kotlin/no/nav/medlemskap/saga/domain/datagrunnlag/udi/GjeldendeOppholdsstatus.kt
+++ src/main/kotlin/no/nav/medlemskap/saga/domain/datagrunnlag/udi/GjeldendeOppholdsstatus.kt
@@ -1,5 +1,12 @@
 package no.nav.medlemskap.saga.domain.datagrunnlag.udi
 
 data class GjeldendeOppholdsstatus(
-    val oppholdstillatelsePaSammeVilkar: OppholdstillatelsePaSammeVilkar?
+    val oppholdstillatelsePaSammeVilkar: OppholdstillatelsePaSammeVilkar?,
+    val eosellerEFTAOpphold: EOSellerEFTAOpphold?,
+    val uavklart: Uavklart?,
+    val ikkeOppholdstillatelseIkkeOppholdsPaSammeVilkarIkkeVisum: IkkeOppholdstillatelseIkkeOppholdsPaSammeVilkarIkkeVisum?
+)
+
+data class Uavklart(
+    val uavklart: Boolean = false
 )
